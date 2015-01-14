61a7120fef38742eb959b8a54c56f001afe0799f
Octavian Nita <mnothro@yahoo.com>
2015-01-14 17:04:13 +0100

    added stronger support for function execution

--- src/main/java/com/webmbt/mscript/Function.java
+++ src/main/java/com/webmbt/mscript/Function.java
@@ -1,6 +1,7 @@
 package com.webmbt.mscript;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,7 +12,7 @@ import java.util.Map;
  * </p>
  * <p>
  * By default, a newly created <em>Function</em> has 0 arity (minimum, as well as maximum). Whenever an implementation
- * is {@link #addImplementation(Method) added}, both arities are updated accordingly and atomically.
+ * is {@link #addImplementation(Object, Method) added}, both arities are updated accordingly and atomically.
  * </p>
  *
  * @author Octavian Theodor Nita (https://github.com/octavian-nita)
@@ -27,7 +28,7 @@ public class Function {
 
     private int maxArity;
 
-    private final Map<Integer, Method> implementations = new HashMap<>();
+    private final Map<Integer, Implementation> implementations = new HashMap<>();
 
     public Function(String name) {
         this(name, null);
@@ -72,20 +73,36 @@ public class Function {
     }
 
     /**
+     * Equivalent to {@link #addImplementation(Object, Method) addImplementation(null, method)}.
+     */
+    public Function addImplementation(Method method) {
+        return addImplementation(null, method);
+    }
+
+    /**
      * <p>
      * An MScript function can have multiple implementations in the form of (eventually overloaded) Java {@link Method
      * methods}. Currently, the requirement is these methods to only accept {@link String} parameters (and no varargs).
-     * Selecting which actual Java method gets called is done based on the number of arguments provided in the MScript
-     * call as well as on the method arity.
+     * Selecting which actual method gets called and on which target object is done based on the number of arguments
+     * provided in the MScript call as well as on the method arity.
      * </p>
      * <p>
      * The name of the added <code>method</code> does not really matter in identifying the function; this provides
      * for increased flexibility (i.e. allows different naming / calling conventions in MScript, etc.).
      * </p>
+     *
+     * @param target can be <code>null</code> if <code>method</code> is static
+     * @param method the actual Java method that can be invoked upon an MScript function call
      */
-    public Function addImplementation(Method method) {
+    public Function addImplementation(Object target, Method method) {
         if (method == null) {
             throw new NullPointerException("cannot add a null function (Java) implementation");
+        }
+        if (target == null) {
+            if (!Modifier.isStatic(method.getModifiers())) {
+                throw new IllegalArgumentException(
+                    "cannot add a function (Java) non-static implementation without a target object");
+            }
         }
 
         // Validate method arguments (currently, only Strings allowed):
@@ -98,7 +115,8 @@ public class Function {
         }
 
         synchronized (implementations) {
-            implementations.put(parameterTypes.length, method); // eventually replaces previously added implementation!
+            // Eventually replace previously added implementation:
+            implementations.put(parameterTypes.length, new Implementation(target, method));
 
             // Update arity, if necessary or if this is the first added implementation:
             if (parameterTypes.length < minArity || implementations.size() == 1) {
@@ -115,5 +133,16 @@ public class Function {
     @Override
     public String toString() {
         return (pluginName == null ? "$" : "$" + pluginName + ".") + name + "(" + minArity + ".." + maxArity + ")";
+    }
+
+    public static class Implementation {
+
+        public final Object target;
+        public final Method method;
+
+        public Implementation(Object target, Method method) {
+            this.target = target;
+            this.method = method;
+        }
     }
 }
